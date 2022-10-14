4525d0b5766d61929fba91c7714d74e9eaa1b1f9
Thomasgme <tomgkme@gmail.com>
2022-10-14 01:17:43 -0700

    hot fix

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * *main - Entry
+ * Return: Always 0
+ */
+int main(void)
+{
+	printf("size of char: %i byte(s)\n" sizeo f(char));
+	printf("size of int: %i byte(s)\n" sizeo f(int));
+	printf("size of long int: %i byte(s)\n" sizeo f(long int));
+	printf("size of long long int: %i byte(s)\n" sizeo f(long long int));
+	printf("size of float: %i byte(s)\n" sizeo f(float));
+	return (0);
+}
