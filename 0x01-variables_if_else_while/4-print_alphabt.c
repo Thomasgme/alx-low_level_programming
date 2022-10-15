d53693f74ce9d11a469308b54de9d1c24e46cfe7
Thomasgme <tomgkme@gmail.com>
2022-10-15 00:01:59 -0700

    hot fix

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio>
+
+/**
+ * main - prnts apha in lower case
+ * Return: Always 0
+ */
+int main(void)
+{
+	char le;
+
+	for (le = 'a'; le <= 'z'; lc++)
+	{
+		putchar(le);
+	}
+	putchar('n');
+
+	return (0);
+}
