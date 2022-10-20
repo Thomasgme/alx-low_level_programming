56bad9cee6776ed7b37a652def3073b9cb314ea4
Thomasgme <tomgkme@gmail.com>
2022-10-20 06:21:09 -0700

    hot fic

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,17 @@
 #include "main.h"
 /**
  * main - return _putchar
- * Return - Always 0
+asidfdsfa * Return - Always 0
  */
 int main(void)
 {
-	 print_alphabet();
+	 char c[] = "_putchar";
+	 int i;
+
+	 for (i = 0; i < 7; i++)
+	 {
+		 _putchar(c[i]);
+	 }
+	 _putchar('\n');
  	 return (0);
 }
