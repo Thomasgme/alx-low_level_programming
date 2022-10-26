1e3ff63103a3977028fa61fb4846f661c742aaf6
Thomasgme <tomgkme@gmail.com>
2022-10-26 00:40:24 -0700

    hot fix

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- * add - Adds two integers.
+ * sum - Adds two integers.
  * @nu1: The first integer to be added.
  * @nu2: The second integer to be added.
  * Return: The result of the addition.
  */
-int add(int nu1, int nu2)
+int sum(int nu1, int nu2)
 {
 	return (nu1 + nu2);
 }
