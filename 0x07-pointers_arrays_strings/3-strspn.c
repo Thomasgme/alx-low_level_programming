0474328d87d28870f4092f4614326d1d7b86391e
Thomasgme <tomgkme@gmail.com>
2022-10-31 03:47:03 -0700

    fh

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,51 +1,38 @@
 #include "main.h"
 /**
- * _strspn - Locates a character in a string
- * @s: This is the main C string to be scanned.
- * @accept: This is the string containing the list of characters to match in s
- * Return: return count
+ * _strspn - function  calculates  the  length (in bytes) of the initial
+ * segment of s which consists entirely of bytes in accept.
+ * @s: string to search in.
+ * @accept: bytes in which to search for.
+ * Return: the number (U_INT) of bytes matching accept.
  */
 unsigned int _strspn(char *s, char *accept)
-
-
 {
-	int i, j;
-	int count = 0;
-	char *str1, *str2;
+	unsigned int i, z, count, checker;
 
-	str1 = s;
-	str2 = accept;
-
-	i = 0;while (str1[i] != '\0')
-		/*Declaring WHILE *s */
+	i = 0;
+	z = 0;
+	checker = 0;
+	count = 0;
+	while (s[i] != '\0')
 	{
-		
-		j = 0;
-		while (str2[j] != '\0')
-			/*Declaring WHILE *accept*/
+		z = 0;
+		checker = 0;
+		while (accept[z] != '\0')
 		{
-			if (str2[j] == str1[i])
-				/*Evaluate condition*/
+			if (accept[z] == s[i])
 			{
 				count++;
-				/*count number*/
+				checker = 1;
+				/* Break to early increase efficiency */
 				break;
 			}
-
-			j++;
-			/*add j+1*/
+			z++;
 		}
 
-		if (s[i] != accept[j])
-			/*If aren't equals*/
-		{
-			break;
-		}
-
+		if (checker == 0)
+			return (count);
 		i++;
-		/*add x+1*/
 	}
-
 	return (count);
-	/*return the value of count*/
 }
