ed202aa8b95f1327ce1de8481089c32f4ab4c2e5
Thomasgme <tomgkme@gmail.com>
2022-11-09 00:18:49 -0800

    hf

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -3,12 +3,14 @@
 #include <string.h>
 #include <ctype.h>
 #include "main.h"
+
 /**
  * main - Program that takes in all integer arguments and returns the sum
  * @argc: Number of command line arguments
  * @argv: Array name
  * Return: 1 if a non-integer is among the passed in arguments, 0 otherwise
  */
+
 int main(int argc, char *argv[])
 {
 	int i, j, length, sum;
@@ -16,9 +18,11 @@ int main(int argc, char *argv[])
 
 	if (argc < 2)
 		printf("0\n");
+
 	else
 	{
 		sum = 0;
+
 		for (i = 1; i < argc; i++)
 		{
 			ptr = argv[i];
