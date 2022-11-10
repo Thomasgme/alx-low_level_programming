21ada3aa4129d9494aaefb9d79323a0b3820a835
Thomasgme <tomgkme@gmail.com>
2022-11-10 04:11:34 -0800

    fix

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * malloc_checked - array for prints a string
+ * @b: number of memory
+ * Return: void
+ */
+{
+	void *p;
+	p = malloc(b);
+
+	if (p == NULL)
+	{
+		exit(98);
+	}
+	return (p);
+}
