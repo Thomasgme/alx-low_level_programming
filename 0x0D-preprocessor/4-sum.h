bda44070cc28d8478ce38a94e5b22c9585dc89ba
Thomasgme <tomgkme@gmail.com>
2022-11-14 22:26:12 -0800

    fix

--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -1,16 +1,11 @@
-#include <stdio.h>
-#include "4-sum.h"
-#include "4-sum.h"
+#ifndef SUM_H
+#define SUM_H
 
 /**
- * main - check the code for ALX School students.
- * Return: Always 0.
+ * File: 4-sum.h
+ * Desc: Header file that defines a function-like macro SUM(x, y)
+ *       that computes the sum of the numbers x and y.
  */
-int main(void)
-{
-	int s;
+#define SUM(x, y) ((x) + (y))
 
-	s = SUM(98, 1024);
-	printf("%d\n", s);
-	return (0);
-}
+#endif
