1f719c22b8e9c69f54684acbb0c87a29c60715a1
Thomasgme <tomgkme@gmail.com>
2022-11-25 04:50:29 -0800

    hf

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -13,10 +13,8 @@ list_t *add_node_end(list_t **head, const char *str)
 
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
-		 return (NULL);
-
+		return (NULL);
 	new->str = strdup(str);
-
 	for (nchar = 0; str[nchar]; nchar++)
 		;
 	new->len = nchar;
